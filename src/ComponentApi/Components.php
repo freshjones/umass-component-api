77c08f3a29f594b83aeb1f60a62055525f7abeed
William Jones <billy@freshjones.com>
2016-06-27 11:21:10 -0400

    adding component class

---
+++ src/ComponentApi/Components.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ComponentApi;
+
+class Components
+{	
+	private $components;
+
+	public function __construct()
+	{
+		$this->initComponents();
+	}
+
+	public function initComponents()
+	{
+		$this->components = array();
+		$this->addComponents();
+	}
+
+	public function addComponents()
+	{
+		$component = new \stdClass();
+		$component->key = 'header';
+		$component->template = '<div>Hello Dude</div>';
+
+		$this->addComponent($component);
+
+	}
+
+	public function addComponent($component)
+	{
+		$this->components[$component->key] = $component;
+	}
+
+	public function getComponent($component)
+	{
+		return $this->components[$component];
+	}
+
+}

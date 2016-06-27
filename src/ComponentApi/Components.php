4b310faa071a2d7d16a1ba3587231349fa5fa7f0
William Jones <billy@freshjones.com>
2016-06-27 11:41:42 -0400

    testing twig install

--- src/ComponentApi/Components.php
+++ src/ComponentApi/Components.php
@@ -5,6 +5,8 @@ namespace ComponentApi;
 class Components
 {	
 	private $components;
+	private $twig;
+	private $loader;
 
 	public function __construct()
 	{
@@ -13,15 +15,23 @@ class Components
 
 	public function initComponents()
 	{
+
+		$this->loader = new \Twig_Loader_Array(array(
+		    'index' => 'Hello {{ name }}!',
+		));
+
+		$this->twig = new \Twig_Environment($this->loader);
+		
 		$this->components = array();
 		$this->addComponents();
 	}
 
 	public function addComponents()
 	{
+
 		$component = new \stdClass();
 		$component->key = 'header';
-		$component->template = '<div>Hello Dude this is great!</div>';
+		$component->template = $this->twig->render('index', array('name' => 'Fabien'));
 
 		$this->addComponent($component);
 
